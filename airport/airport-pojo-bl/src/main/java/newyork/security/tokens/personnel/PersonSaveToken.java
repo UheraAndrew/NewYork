cce9265605f33d51d155f37381d4511486947476
SofiyaHevorhyan <avisventus@gmail.com>
2019-10-24 02:12:44 +0300

    Initial project upload.

---
+++ airport/airport-pojo-bl/src/main/java/newyork/security/tokens/personnel/PersonSaveToken.java
@@ -0,0 +1,13 @@
+package newyork.security.tokens.personnel;
+
+import ua.com.fielden.platform.entity.annotation.KeyTitle;
+
+/**
+ * Save person security token.
+ *
+ * @author TG Team
+ *
+ */
+@KeyTitle(value = "Person save", desc = "Controls permission to save personnel changes.")
+public class PersonSaveToken extends PersonnelReviewToken {
+}
---
+++ airport/airport-web-ui/src/main/java/newyork/main/menu/personnel/MiPerson.java
@@ -0,0 +1,16 @@
+package newyork.main.menu.personnel;
+
+import ua.com.fielden.platform.entity.annotation.EntityType;
+import ua.com.fielden.platform.ui.menu.MiWithConfigurationSupport;
+import newyork.personnel.Person;
+
+/**
+ * Main menu item representing an entity centre for {@link Person}.
+ *
+ * @author Generated
+ *
+ */
+@EntityType(Person.class)
+public class MiPerson extends MiWithConfigurationSupport<Person> {
+
+}
