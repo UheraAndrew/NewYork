3c4083e4f8c98b48b3a163ed9a5258366653aade
SofiyaHevorhyan <avisventus@gmail.com>
2019-10-31 17:44:23 +0200

    Adding Asset Type model and UI

---
+++ airport/airport-web-ui/src/main/java/newyork/main/menu/tablescodes/assets/MiAssetType.java
@@ -0,0 +1,15 @@
+package newyork.main.menu.tablescodes.assets;
+
+import ua.com.fielden.platform.entity.annotation.EntityType;
+import ua.com.fielden.platform.ui.menu.MiWithConfigurationSupport;
+import newyork.tablescodes.assets.AssetType;
+/**
+ * Main menu item representing an entity centre for {@link AssetType}.
+ *
+ * @author Developers
+ *
+ */
+@EntityType(AssetType.class)
+public class MiAssetType extends MiWithConfigurationSupport<AssetType> {
+
+}
