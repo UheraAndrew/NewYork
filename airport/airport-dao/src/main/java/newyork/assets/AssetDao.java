675bc543148d6c4c1fcce9062ade1cf46b5a7eef
SofiyaHevorhyan <avisventus@gmail.com>
2019-12-07 16:51:07 +0200

    #5 Implemented a weak automatic key assignment.

--- airport/airport-dao/src/main/java/newyork/assets/AssetDao.java
+++ airport/airport-dao/src/main/java/newyork/assets/AssetDao.java
@@ -1,15 +1,17 @@
 package newyork.assets;
 
-import com.google.inject.Inject;
-
 import java.util.Collection;
 import java.util.List;
 
-import ua.com.fielden.platform.entity.fetch.IFetchProvider;
-import ua.com.fielden.platform.dao.annotations.SessionRequired;
+import com.google.inject.Inject;
+
 import ua.com.fielden.platform.dao.CommonEntityDao;
-import ua.com.fielden.platform.entity.query.IFilter;
+import ua.com.fielden.platform.dao.annotations.SessionRequired;
 import ua.com.fielden.platform.entity.annotation.EntityType;
+import ua.com.fielden.platform.entity.fetch.IFetchProvider;
+import ua.com.fielden.platform.entity.query.IFilter;
+import ua.com.fielden.platform.keygen.IKeyNumber;
+import ua.com.fielden.platform.keygen.KeyNumber;
 /**
  * DAO implementation for companion object {@link IAsset}.
  *
@@ -18,10 +20,37 @@ import ua.com.fielden.platform.entity.annotation.EntityType;
  */
 @EntityType(Asset.class)
 public class AssetDao extends CommonEntityDao<Asset> implements IAsset {
+    public static final String DEFAULT_ASSET_NUMBER = "NEXT NUMBER WILL BE GENERATED UPON SAVE";
 
     @Inject
     public AssetDao(final IFilter filter) {
         super(filter);
+    }
+    
+    @Override
+    @SessionRequired
+    public Asset save(final Asset asset) {
+        // TODO: implement a solution for a failed transaction where ID was already assigned 
+        // what if in this place we have an exception?
+        // like the asset with number 3 is already created but your number 3 is already assigned
+        // conflict!
+        // would you check if your asset is persisted but also if you value is not the default one?
+        // to revert the number assigned
+        if (!asset.isPersisted()) {
+            final IKeyNumber coKeyNumber = co(KeyNumber.class);
+            final Integer nextNumber = coKeyNumber.nextNumber("ASSET_NUMBER");
+            asset.setNumber(nextNumber.toString());
+        }
+        // TODO: length of six, to have not numbers but strings in form like 000001 or 000101
+        return super.save(asset);
+    }
+    
+    @Override
+    public Asset new_() {
+        final Asset asset = super.new_();
+        asset.setNumber(DEFAULT_ASSET_NUMBER);
+        
+        return asset;
     }
 
     @Override
